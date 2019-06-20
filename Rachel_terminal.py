ae7c6cb3f647a9fff168b185dabc6c4fa5ff23c2
Emmanuel <manray914@gmail.com>
2019-06-20 02:06:29 -0400

    added functions and changed layout

--- Rachel_terminal.py
+++ Rachel_terminal.py
@@ -1,38 +1,82 @@
 import time
 
-inp='none'
-inp2='none'
-yes=['yes','y']
-no=['no','n']
-answers=['yes','y','no','n']
+yes = ['yes','y']
+no = ['no','n']
+answers = ['yes','y','no','n']
 
-while inp.lower() not in yes:
-    inp=input('\nAre you Rachel? ')
-    if inp.lower() in yes:
+def pause(seconds=.7):
+    time.sleep(seconds)
+
+def question(string):
+    inp="none"
+    while inp.lower() not in answers:
+        print (string)
+        inp=input("")
+        if inp.lower() in yes:
+            answer=True
+        elif inp.lower() in no:
+            answer=False
+        else:
+            print("\nPlease enter Yes or No")
+    return answer
+
+answer1 = False
+while answer1 == False:
+    answer1 = question('\nAre you Rachel?')
+    if answer1 == True:
         print('\nCongratulations you are beautiful!')
-        time.sleep(1)
-        while inp2.lower() not in no:
-            inp2=input('\nAre you hungry? ')
-            if inp2.lower() in yes:
-                print('\nOh god... ')
-                time.sleep(1)
-                print('.....')
-                time.sleep(1)
-                print("\nPlease don't hurt me!")
-                print("*Manny runs in fear for his life*")
-            elif inp2.lower() in no:
-                print("\nOkay good!, Let's continue! :D ")
-            else:
-                print("\nYes or no only please!.. I don't need your life story")
-    elif inp.lower() in no:
+        pause(1.2)
+    elif answer1 == False:
         print('\nYou are probably ugly')
-        time.sleep(1)
+        pause()
         print('\n..sorry')
-        time.sleep(1)
+        pause()
         print('\nGoodbye!')
-        time.sleep(.5)
-    else:
-        print('\nPlease answer yes or no!')
+        pause(1.5)
 
-time.sleep(1)
+answer2 = True
+while answer2 == True:
+    answer2 = question('\nAre you hungry? ')
+    if answer2 == True:
+        print('\nOh god... ')
+        pause()
+        print('.....')
+        pause()
+        print("\nPlease don't hurt me!")
+        print("*Manny runs in fear for his life*")
+        pause(2)
+    if answer2 == False:
+        print("\nOkay good!, Let's continue! :D ")
+        pause(2)
+        print("...")
+        pause(2)
+        print('\nBut first')
+        pause(1)
+        print('...')
+        pause(1)
+        print('\nI do have to make sure you are the "real" Rachel,')
+        pause(2)
+        print('this is a very sensitive matter, afterall')
+        pause(2.5)
+
+answer3 = "none"
+fav_color = ["purple","2"]
+color_options = ["(1)Red ","(2)Purple ","(3)Pink ","(4)Orange"]
+colors = ['1','2','3','4','red','purple','pink','orange']
+while answer3 not in fav_color:
+    print('\n\nWhat is your favorite color? (Type the color or number)')
+    print(*color_options)
+    answer3 = input('')
+    pause()
+    if str(answer3.lower()) in fav_color:
+        print('\nWow you really know your stuff!')
+        pause()
+    if answer3.lower() not in colors:
+        print("\nThat's not even an option!!")
+        pause()
+    elif str(answer3.lower()) not in fav_color:
+        print('\nI knew you were an imposter!!')
+        pause()
+
+pause()
 input('\n\nPress enter to exit!')
